2909712bd769f0d70084c1510f14ef0e708b30bb
Olingo Maxwell <69717524+olingomaxwell1999@users.noreply.github.com>
2025-03-09 00:16:40 +0300

    removed the signin page and implemented clerk by updating the midlleware file.. removed member login page since we are using clerk

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,22 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  // Public routes that don't require authentication
-  publicRoutes: [
-    "/",
-    "/about",
-    "/membership",
-    "/events",
-    "/resources",
-    "/advocacy",
-    "/contact"
-  ],
-  
-  // Routes that can be accessed while signed in or not
-  ignoredRoutes: [
-    "/api/public"
-  ],
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+//chatbot is protected
+const isProtectedRoute = createRouteMatcher(["/member-dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    //redirect to sign in page
+    return redirectToSignIn();
+  }
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
