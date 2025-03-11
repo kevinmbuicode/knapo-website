2c70f81f677903189a6c534d3e4969c6df56fc9f
Olingo Maxwell <69717524+olingomaxwell1999@users.noreply.github.com>
2025-03-11 04:57:06 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/about",
-    "/membership",
-    "/events",
-    "/resources",
-    "/advocacy",
-    "/contact",
-    "/api/(.*)",
-    "/sign-in",
-    "/sign-up"
-  ],
-  ignoredRoutes: [
-    "/(api|trpc)/(.*)"
-  ]
-});
+const isProtectedRoute = createRouteMatcher(['/member-dashboard(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
